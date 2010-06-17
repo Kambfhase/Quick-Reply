92f4a5bc97e0970b1342072b5ad5a89937f27c41
Kambfhase <kambfhase@hotmail.de>
2010-06-18 00:28:25 +0200

    das Versionsattribut enthaellt nun auch den Zielbrowser

--- src/header.js
+++ src/header.js
@@ -1,14 +1,17 @@
 // ==UserScript==
 // @name        Quick-Reply by Kambfhase
-// @version     2.6.2
 // @author      Kambfhase
 #ifdef GREASEMONKEY
 // @description ein Quick-Reply Script für mods.de - Greasemonkey Version
+// @version     2.6.2 - Greasemonkey Version
 #endif
 #ifdef CHROME
 // @description ein Quick-Reply Script für mods.de - Chrome Version
+// @version     2.6.2 - Chrome Version
 #endif
 // @include     http://forum.mods.de/bb/thread.php?*
 // @include     http://forum.mods.de/bb//thread.php?*
+// @match     http://forum.mods.de/bb/thread.php?*
+// @match     http://forum.mods.de/bb//thread.php?*
 // @run-at    document-end
 // ==/UserScript==
