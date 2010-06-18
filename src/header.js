bbbfc95a5dd7c326e9b600f5c87b952acdd8897f
Kambfhase <kambfhase@hotmail.de>
2010-06-18 18:41:39 +0200

    auf v2.6.3 erhoeht.

--- src/header.js
+++ src/header.js
@@ -3,11 +3,11 @@
 // @author      Kambfhase
 #ifdef GREASEMONKEY
 // @description ein Quick-Reply Script für mods.de - Greasemonkey Version
-// @version     2.6.2 - Greasemonkey Version
+// @version     2.6.3 - Greasemonkey Version
 #endif
 #ifdef CHROME
 // @description ein Quick-Reply Script für mods.de - Chrome Version
-// @version     2.6.2 - Chrome Version
+// @version     2.6.3 - Chrome Version
 #endif
 // @include     http://forum.mods.de/bb/thread.php?*
 // @include     http://forum.mods.de/bb//thread.php?*
