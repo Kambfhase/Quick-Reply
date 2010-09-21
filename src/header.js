85a1eb7151c36659f1759da429d4420595b98861
Kambfhase <kambfhase@hotmail.de>
2010-09-21 15:48:56 +0200

    build v2.6.4

--- src/header.js
+++ src/header.js
@@ -3,11 +3,11 @@
 // @author      Kambfhase
 #ifdef GREASEMONKEY
 // @description ein Quick-Reply Script für mods.de - Greasemonkey Version
-// @version     2.6.3 - Greasemonkey Version
+// @version     2.6.4 - Greasemonkey Version
 #endif
 #ifdef CHROME
 // @description ein Quick-Reply Script für mods.de - Chrome Version
-// @version     2.6.3 - Chrome Version
+// @version     2.6.4 - Chrome Version
 #endif
 // @include     http://forum.mods.de/bb/thread.php?*
 // @include     http://forum.mods.de/bb//thread.php?*
