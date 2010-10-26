8cbeeec09e90bde3c89b136ddac0481cf4659984
Kambfhase <kambfhase@hotmail.de>
2010-10-26 13:52:02 +0200

    Version auf 2.6.5 erhöht

--- src/header.js
+++ src/header.js
@@ -3,11 +3,11 @@
 // @author      Kambfhase
 #ifdef GREASEMONKEY
 // @description ein Quick-Reply Script für mods.de - Greasemonkey Version
-// @version     2.6.4 - Greasemonkey Version
+// @version     2.6.5 - Greasemonkey Version
 #endif
 #ifdef CHROME
 // @description ein Quick-Reply Script für mods.de - Chrome Version
-// @version     2.6.4 - Chrome Version
+// @version     2.6.5 - Chrome Version
 #endif
 // @include     http://forum.mods.de/bb/thread.php?*
 // @include     http://forum.mods.de/bb//thread.php?*
