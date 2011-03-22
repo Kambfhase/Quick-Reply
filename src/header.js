7c16723ddaf24c14763c474e50e07b9c12e6eea3
Kambfhase <kambfhase@hotmail.de>
2011-03-22 13:49:28 +0100

    version bump nach 2.6.9

--- src/header.js
+++ src/header.js
@@ -3,15 +3,15 @@
 // @author      Kambfhase
 #ifdef GREASEMONKEY
 // @description ein Quick-Reply Script für mods.de - Greasemonkey Version
-// @version     2.6.8 - Greasemonkey Version
+// @version     2.6.9 - Greasemonkey Version
 #endif
 #ifdef CHROME
 // @description ein Quick-Reply Script für mods.de - Chrome Version
-// @version     2.6.8 - Chrome Version
+// @version     2.6.9 - Chrome Version
 #endif
 #ifdef OPERA
 // @description ein Quick-Reply Script für mods.de - Opera Version
-// @version     2.6.8 - Opera Version
+// @version     2.6.9 - Opera Version
 #endif
 // @include     http://forum.mods.de/bb/thread.php?*
 // @include     http://forum.mods.de/bb//thread.php?*
