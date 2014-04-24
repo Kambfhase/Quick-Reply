ddc7ac437a74796ec02ec6568996147a15c67ee3
Kambfhase <kambfhase@hotmail.de>
2014-04-24 20:48:29 +0200

    bump version number

--- rewrite.js
+++ rewrite.js
@@ -2,7 +2,7 @@
 // @name        Quick-Reply by Kambfhase
 // @author      Kambfhase
 // @description ein Quick-Reply Script fuer mods.de
-// @version     2.7.2
+// @version     2.7.3
 // @include     http://forum.mods.de/bb/thread.php?*
 // @include     http://forum.mods.de/bb//thread.php?*
 // ==/UserScript==
